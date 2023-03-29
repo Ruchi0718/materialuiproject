24a290c79a3b8aea943fbf4a16f48d58df76f995
Ruchi0718 <127466336+Ruchi0718@users.noreply.github.com>
2023-03-29 13:18:10 +0530

    feature: homepage

--- src/component/form/form.js
+++ src/component/form/form.js
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
-import { Box, Grid, Typography, Button, Checkbox } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Typography,
+  Button,
+  Checkbox,
+  FormControl,
+} from "@mui/material";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
+// import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import { Container } from "@mui/system";
@@ -39,193 +46,217 @@ const Form = () => {
     setCountry(event.target.value);
   };
   return (
-
-      <Box sx={{ height: 620 }}>
-        <Container>
-          <Box
-            display="flex"
-            flexDirection={"column"}
-            alignItems={{ lg: "center", xs: "center" }}
-            justifyContent={"center"}
-            sx={{
-              // border: "2px solid red",
-              padding: 2,
-              // margin: "auto",
-              width: { lg: 450, xs: 330 },
-              height: { lg: 550 },
-              marginTop: 4,
-              backgroundColor: "#F8F8F8",
-            }}
-          >
-            <Grid container>
-              <Grid
-                textAlign={"center"}
-                justifyContent={"center"}
-                ml={{ lg: 14, xs: 7, sm: 5, md: 6 }}
-                sx={{
-                  fontSize: "xx-large",
-                  fontWeight: "bold",
-                  color: "#FF311A",
-                }}
-                item
-                xs={4}
-              >
-                SEARCH
-              </Grid>
-              <Grid
-                textAlign={"center"}
-                justifyContent={"center"}
-                ml={{ lg: 1, xs: 5 }}
-                sx={{ fontSize: "xx-large", fontWeight: "bold", color: "grey" }}
-                item
-                xs={3}
-              >
-                HERE
-              </Grid>
+    <Box sx={{ height: 620 }}>
+      <Container>
+        <Box
+          display="flex"
+          flexDirection={"column"}
+          alignItems={{ lg: "center", xs: "center" }}
+          justifyContent={"center"}
+          sx={{
+            // border: "2px solid red",
+            padding: 2,
+            // margin: "auto",
+            width: { lg: 450, xs: 330 },
+            height: { lg: 550 },
+            marginTop: 4,
+            backgroundColor: "#F8F8F8",
+          }}
+        >
+          <Grid container>
+            <Grid
+              textAlign={"center"}
+              justifyContent={"center"}
+              ml={{ lg: 14, xs: 7, sm: 5, md: 6 }}
+              sx={{
+                fontSize: "xx-large",
+                fontWeight: "bold",
+                color: "#FF311A",
+              }}
+              item
+              xs={4}
+            >
+              SEARCH
             </Grid>
-            <Typography sx={{ marginLeft: { lg: "70%", xs: "40%" } }}>
-              <u>Profile ID Search</u>
-            </Typography>
-            <br></br>
-            <Grid m={1} container xs={12}>
-              <Grid item xs={4}>
-                <Typography align="center">Looking For </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <FormControl sx={{ width: { lg: 275, xs: 200 } }} size="small">
-                  <InputLabel id="demo-simple-select-label">
-                    Looking For
-                  </InputLabel>
-                  <Select fullWidth value={lookingFor} onChange={handleChange}>
-                    <MenuItem value={"bride"}>Bride</MenuItem>
-                    <MenuItem value={"groom"}>Groom</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
+            <Grid
+              textAlign={"center"}
+              justifyContent={"center"}
+              ml={{ lg: 1, xs: 5 }}
+              sx={{ fontSize: "xx-large", fontWeight: "bold", color: "grey" }}
+              item
+              xs={3}
+            >
+              HERE
             </Grid>
-            <Grid m={1} container xs={12}>
-              <Grid item xs={4}>
-                <Typography align="center">Age</Typography>
-              </Grid>
-              <Grid item xs={3}>
-                <FormControl sx={{ width: { lg: 110, xs: 78 } }} size="small">
-                  <InputLabel id="demo-simple-select-label">From</InputLabel>
-                  <Select value={age} onChange={handleChangeAge}>
-                    <MenuItem value={10}>18</MenuItem>
-                    <MenuItem value={20}>19</MenuItem>
-                    <MenuItem value={30}>20</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-              <Grid item xs={1}>
-                <Typography align="center">To</Typography>
-              </Grid>
-              <Grid ml={{ lg: 2, xs: 0 }} item xs={3}>
-                <FormControl sx={{ width: { lg: 110, xs: 78 } }} size="small">
-                  <InputLabel id="demo-simple-select-label">To</InputLabel>
-                  <Select
-                    value={toage}
-                    onChange={handleChangetoAge}
-                    labelId="demo-select-small"
-                    id="demo-select-small"
-                  >
-                    <MenuItem value={10}>25</MenuItem>
-                    <MenuItem value={20}>28</MenuItem>
-                    <MenuItem value={30}>30</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
+          </Grid>
+          <Typography sx={{ marginLeft: { lg: "70%", xs: "40%" } }}>
+            <u>Profile ID Search</u>
+          </Typography>
+          <br></br>
+          <Grid m={1} container xs={12}>
+            <Grid item xs={4}>
+              <Typography align="center">Looking For </Typography>
             </Grid>
-            <Grid m={1} container xs={12}>
-              <Grid item xs={4}>
-                <Typography align="center">Religion </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <FormControl sx={{ width: { lg: 275, xs: 200 } }} size="small">
-                  <InputLabel id="demo-simple-select-label">
-                    Religion
-                  </InputLabel>
-                  <Select
-                    fullWidth
-                    value={religion}
-                    onChange={handleChangereligion}
-                  >
-                    <MenuItem value={"hindu"}>Hindu</MenuItem>
-                    <MenuItem value={"muslim"}>Muslim</MenuItem>
-                    <MenuItem value={"christian"}>Christian</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
+            <Grid item xs={6}>
+              <FormControl sx={{ width: { lg: 275, xs: 200 } }} size="small">
+                <InputLabel id="demo-simple-select-label">
+                  Looking For
+                </InputLabel>
+                <Select
+                  fullWidth
+                  value={lookingFor}
+                  onChange={handleChange}
+                  labelId="demo-select-small"
+                  id="demo-select-small"
+                  label="Lookingfor"
+                >
+                  <MenuItem value={"bride"}>Bride</MenuItem>
+                  <MenuItem value={"groom"}>Groom</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
-            <Grid m={1} container xs={12}>
-              <Grid item xs={4}>
-                <Typography align="center">Caste </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <FormControl sx={{ width: { lg: 275, xs: 200 } }} size="small">
-                  <InputLabel id="demo-simple-select-label">Caste</InputLabel>
-                  <Select fullWidth value={caste} onChange={handleChangeCaste}>
-                    <MenuItem value={"general"}>General</MenuItem>
-                    <MenuItem value={"obc"}>Obc</MenuItem>
-                    <MenuItem value={"st/sc"}>St/Sc</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
+          </Grid>
+          <Grid m={1} container xs={12}>
+            <Grid item xs={4}>
+              <Typography align="center">Age</Typography>
             </Grid>
-            <Grid m={1} container xs={12}>
-              <Grid item xs={4}>
-                <Typography align="center">Community </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <FormControl sx={{ width: { lg: 275, xs: 200 } }} size="small">
-                  <InputLabel id="demo-simple-select-label">
-                    Community
-                  </InputLabel>
-                  <Select
-                    fullWidth
-                    value={community}
-                    onChange={handleChangeCommunity}
-                  >
-                    <MenuItem value={"general"}>General</MenuItem>
-                    <MenuItem value={"obc"}>Obc</MenuItem>
-                    <MenuItem value={"st/sc"}>St/Sc</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-            </Grid>{" "}
-            <Grid m={1} container xs={12}>
-              <Grid item xs={4}>
-                <Typography align="center">Country </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <FormControl sx={{ width: { lg: 275, xs: 200 } }} size="small">
-                  <InputLabel id="demo-simple-select-label">Country</InputLabel>
-                  <Select
-                    fullWidth
-                    value={country}
-                    onChange={handleChangeCountry}
-                  >
-                    <MenuItem value={"India"}>India</MenuItem>
-                    <MenuItem value={"canada"}>Canada</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
+            <Grid item xs={3}>
+              <FormControl sx={{ width: { lg: 110, xs: 78 } }} size="small">
+                <InputLabel id="demo-simple-select-label">From</InputLabel>
+                <Select
+                  value={age}
+                  onChange={handleChangeAge}
+                  labelId="demo-select-small"
+                  id="demo-select-small"
+                  label="From"
+                >
+                  <MenuItem value={10}>18</MenuItem>
+                  <MenuItem value={20}>19</MenuItem>
+                  <MenuItem value={30}>20</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
-            <Grid sx={{ marginLeft: "20%" }}>
-              <FormControlLabel
-                control={<Checkbox defaultChecked />}
-                label="With Photo Advance Search"
-              />
+            <Grid item xs={1}>
+              <Typography align="center">To</Typography>
             </Grid>
-            <Grid m={2} container xs={12}>
-              <Button sx={{ marginLeft: "30%" }} variant="contained">
-                Search
-              </Button>
+            <Grid ml={{ lg: 2, xs: 0 }} item xs={3}>
+              <FormControl sx={{ width: { lg: 110, xs: 78 } }} size="small">
+                <InputLabel id="demo-simple-select-label">To</InputLabel>
+                <Select
+                  value={toage}
+                  onChange={handleChangetoAge}
+                  labelId="demo-select-small"
+                  id="demo-select-small"
+                  label="To"
+                >
+                  <MenuItem value={10}>25</MenuItem>
+                  <MenuItem value={20}>28</MenuItem>
+                  <MenuItem value={30}>30</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
-          </Box>
-        </Container>
-      </Box>
-
+          </Grid>
+          <Grid m={1} container xs={12}>
+            <Grid item xs={4}>
+              <Typography align="center">Religion </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl sx={{ width: { lg: 275, xs: 200 } }} size="small">
+                <InputLabel id="demo-simple-select-label">Religion</InputLabel>
+                <Select
+                  labelId="demo-select-small"
+                  id="demo-select-small"
+                  label="Religion"
+                  fullWidth
+                  value={religion}
+                  onChange={handleChangereligion}
+                >
+                  <MenuItem value={"hindu"}>Hindu</MenuItem>
+                  <MenuItem value={"muslim"}>Muslim</MenuItem>
+                  <MenuItem value={"christian"}>Christian</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Grid m={1} container xs={12}>
+            <Grid item xs={4}>
+              <Typography align="center">Caste </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl sx={{ width: { lg: 275, xs: 200 } }} size="small">
+                <InputLabel id="demo-simple-select-label">Caste</InputLabel>
+                <Select
+                  fullWidth
+                  value={caste}
+                  onChange={handleChangeCaste}
+                  labelId="demo-select-small"
+                  id="demo-select-small"
+                  label="Caste"
+                >
+                  <MenuItem value={"general"}>General</MenuItem>
+                  <MenuItem value={"obc"}>Obc</MenuItem>
+                  <MenuItem value={"st/sc"}>St/Sc</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Grid m={1} container xs={12}>
+            <Grid item xs={4}>
+              <Typography align="center">Community </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl sx={{ width: { lg: 275, xs: 200 } }} size="small">
+                <InputLabel id="demo-simple-select-label">Community</InputLabel>
+                <Select
+                  labelId="demo-select-small"
+                  id="demo-select-small"
+                  label="Community"
+                  fullWidth
+                  value={community}
+                  onChange={handleChangeCommunity}
+                >
+                  <MenuItem value={"general"}>General</MenuItem>
+                  <MenuItem value={"obc"}>Obc</MenuItem>
+                  <MenuItem value={"st/sc"}>St/Sc</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>{" "}
+          <Grid m={1} container xs={12}>
+            <Grid item xs={4}>
+              <Typography align="center">Country </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl sx={{ width: { lg: 275, xs: 200 } }} size="small">
+                <InputLabel id="demo-simple-select-label">Country</InputLabel>
+                <Select
+                 labelId="demo-select-small"
+                 id="demo-select-small"
+                 label="Country"
+                  fullWidth
+                  value={country}
+                  onChange={handleChangeCountry}
+                >
+                  <MenuItem value={"India"}>India</MenuItem>
+                  <MenuItem value={"canada"}>Canada</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Grid sx={{ marginLeft: "20%" }}>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="With Photo Advance Search"
+            />
+          </Grid>
+          <Grid m={2} container xs={12}>
+            <Button sx={{ marginLeft: "30%" }} variant="contained">
+              Search
+            </Button>
+          </Grid>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
